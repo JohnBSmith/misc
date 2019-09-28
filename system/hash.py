3d792e1adfbc5d2080baffe5d6d9f47a256969a7
finn <finn@c1>
2019-09-28 12:11:46 +0200

    update

--- system/hash.py
+++ system/hash.py
@@ -15,34 +15,38 @@ def read(path):
     with open(path,"rb") as file:
         return file.read()
 
-def list_files(root):
-    a = []
-    for dirpath, dnames, fnames in os.walk(root):
-        for f in fnames:
-            path = os.path.join(dirpath,f)
-            a.append(path)
-    a.sort()
-    return a
-
 def hash_file(path):
     data = read(path)
     h = new_hasher()
     h.update(data)
     return h
 
-def hash_rec(root):
+def hash_rec(path):
     h = new_hasher()
-    a = []
-    for path in list_files(root):
-        data = hash_file(path).digest()
-        h.update(data)
+    if os.path.isfile(path):
+        h = new_hasher()
+        h.update(read(path))
+        h.update(path.encode("utf-8"))
+    elif os.path.isdir(path):
+        a = []
+        for item in os.listdir(path):
+            a.append(os.path.join(path,item))
+        a.sort()
+        for item in a:
+            hrec = hash_rec(item)
+            h.update(hrec.digest())
+        h.update(path.encode("utf-8"))
+    else:
+        eprint("Strange file detected: {}".format(path))
+        sys.exit(1)
     return h
 
 path = sys.argv[1]
 if os.path.isfile(path):
     print(hash_file(path).hexdigest())
 elif os.path.isdir(path):
-    print(hash_rec(path).hexdigest())
+    os.chdir(path)
+    print(hash_rec("./").hexdigest())
 else:
     eprint("Error: path '{}' cannot be accessed.".format(path))
 
