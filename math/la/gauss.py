8fb519e71517ba4bad2156e66366f909586c7251
finn <finn.bcd@gmail.com>
2022-01-30 15:20:07 +0100

    update

--- math/la/gauss.py
+++ math/la/gauss.py
@@ -8,17 +8,35 @@ def div(x, y):
 
 def format_vec(v, f = str):
     return "[{}]".format(", ".join(f(r) for r in v))
-    
-def row_str(A, j):
-    if isinstance(A[0], list):
-        return "".join(["{:>8}".format(str(x)) for x in A[j]])
-    else:
-        return "{:>8}".format(str(A[j]))
 
-def print_system(A, B, n):
-    for j in range(0, n):
-        print(row_str(A, j), "|", row_str(B, j))
-    print()
+def max_space_col(A, j, f):
+    m = max(len(f(A[i][j])) for i in range(len(A)))
+    return 1 + max(6, m)
+
+def max_space(A, f):
+    if isinstance(A[0], list):
+        return [max_space_col(A, j, f) for j in range(len(A[0]))]
+    else:
+        return 1 + max(6, max(len(f(x)) for x in A))
+
+def row_str(A, i, L, f):
+    if isinstance(A[0], list):
+        r = range(0, len(A[i]))
+        a = ["{:>{}}".format(f(A[i][j]), L[j]) for j in r]
+        return "".join(a)
+    else:
+        return "{:>{}}".format(f(A[i]), L)
+
+def printer(n, f):
+    def print_system(A, B):
+        sA = max_space(A, f)
+        sB = max_space(B, f)
+        for i in range(0, n):
+            rA = row_str(A, i, sA, f)
+            rB = row_str(B, i, sB, f)
+            print("{} |{}".format(rA, rB))
+        print()
+    return print_system
 
 def mul_matrix_vec(A, v):
     return [sum(A[i][j]*v[j] for j in range(0, len(v)))
@@ -47,9 +65,10 @@ def pivoting(A, B, n, j):
     B[j], B[k] = B[k], B[j]
     return m
 
-def gauss_jordan(A, B, n):
+def gauss_jordan(A, B, n, f = str):
+    print_system = printer(n, f)
     print("Lineares Gleichungssystem:")
-    print_system(A, B, n)
+    print_system(A, B)
     zero_pivot = False
     for j in range(0, n):
         pivot = pivoting(A, B, n, j)
@@ -57,7 +76,7 @@ def gauss_jordan(A, B, n):
             zero_pivot = True
             break
         print("Zeilenvertauschung:")
-        print_system(A, B, n)
+        print_system(A, B)
         B[j] = mul_scalar_vec(1/A[j][j], B[j])
         A[j] = mul_scalar_vec(1/A[j][j], A[j])
         for i in range(j + 1, n):
@@ -65,13 +84,13 @@ def gauss_jordan(A, B, n):
                 B[i] = add_vec(1/A[i][j], B[i], -1, B[j])
                 A[i] = add_vec(1/A[i][j], A[i], -1, A[j])
         print("Schritt:")
-        print_system(A, B, n)
+        print_system(A, B)
     for i in range(0, n - 1):
         for j in range(i + 1, n):
             B[i] = add_vec(1, B[i], -A[i][j], B[j])
             A[i] = add_vec(1, A[i], -A[i][j], A[j])
         print("Schritt zur reduzierten Stufenform:")
-        print_system(A, B, n)
+        print_system(A, B)
     if zero_pivot:
         print("Die Matrix ist singulär.")
     return B
@@ -89,5 +108,4 @@ def invert(A):
     A = [[Fraction(r) for r in x] for x in A]
     B = unit_matrix(len(A), Fraction)
     X = gauss_jordan(A, B, len(A))
-
 
