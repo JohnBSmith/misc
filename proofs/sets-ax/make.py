f58cb65fc1f37cfab9a2f8a8794c7bf18d57297e
finn <finn.bcd@gmail.com>
2023-08-15 01:02:51 +0200

    update

--- proofs/sets-ax/make.py
+++ proofs/sets-ax/make.py
@@ -12,6 +12,7 @@ def build(modules):
     if len(argv) == 2 and argv[1] == "clean":
         for name in modules:
             run(["rm", name + ".vo"])
+            run(["rm", "." + name + ".aux"])
     else:
         for name in modules:
             coqc(name)
