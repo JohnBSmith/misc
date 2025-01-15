974fe6ba3166623730977a2264fa25502e89203f
finn <finn.bcd@gmail.com>
2025-01-15 17:13:57 +0100

    update

--- cs/imp/proofs/imp.v
+++ cs/imp/proofs/imp.v
@@ -48,8 +48,8 @@ Fixpoint evB (b: Bexpr) (s: State): bool :=
   | bor b1 b2 => orb (evB b1 s) (evB b2 s)
   end.
 
-Definition variant (s: State) (X: Loc) (n: Z): State :=
-  fun Y => if Nat.eqb X Y then n else s Y.
+Definition variant (s: State) (X: Loc) (a: Aexpr): State :=
+  fun Y => if Nat.eqb X Y then evA a s else s Y.
 
 Fixpoint iter (ev: State -> State -> Prop)
 (N: nat) (b: Bexpr) (s s1: State): Prop :=
@@ -63,7 +63,7 @@ Fixpoint iter (ev: State -> State -> Prop)
 Fixpoint evC (c: Com) (s s1: State): Prop :=
   match c with
   | Skip => s1 = s
-  | Assign X a => s1 = variant s X (evA a s)
+  | Assign X a => s1 = variant s X a
   | Seq c1 c2 => exists s0, evC c1 s s0 /\ evC c2 s0 s1
   | If b c1 c2 => if evB b s then evC c1 s s1 else evC c2 s s1
   | While b c => exists N, iter (evC c) N b s s1
@@ -73,19 +73,19 @@ Definition Assertion := State -> Prop.
 Definition sat (s: State) (A: Assertion): Prop := A s.
 
 Definition valid (A: Assertion) (c: Com) (B: Assertion) :=
-  forall s, sat s A -> forall s', evC c s s' -> sat s' B.
+  forall s, sat s A -> forall s1, evC c s s1 -> sat s1 B.
 
 Definition subst (A: Assertion) (X: Loc) (a: Aexpr): Assertion :=
-  fun s => A (variant s X (evA a s)).
+  fun s => A (variant s X a).
 
-Theorem rule_skip_is_valid A:
+Theorem skip_intro_is_valid A:
   valid A Skip A.
 Proof.
   unfold valid. intros s hs. intros s1 hs1.
   simpl evC in hs1. rewrite hs1. exact hs.
 Qed.
 
-Theorem rule_assign_is_valid A X a:
+Theorem assign_intro_is_valid A X a:
   valid (subst A X a) (Assign X a) A.
 Proof.
   unfold valid. intros s hs. intros s1 hs1.
@@ -94,7 +94,7 @@ Proof.
   rewrite hs1. exact hs.
 Qed.
 
-Theorem rule_seq_is_valid A B C c1 c2:
+Theorem seq_intro_is_valid A B C c1 c2:
   valid A c1 B -> valid B c2 C -> valid A (Seq c1 c2) C.
 Proof.
   intros h1 h2. unfold valid. intros s hs. intros s2 hs2.
@@ -108,7 +108,7 @@ Qed.
 Definition Conj (A: Assertion) (b: Bexpr) :=
   fun s => sat s A /\ evB b s = true.
 
-Theorem rule_if_is_valid A b C c1 c2:
+Theorem if_intro_is_valid A b C c1 c2:
   valid (Conj A b) c1 C ->
   valid (Conj A (bnot b)) c2 C ->
   valid A (If b c1 c2) C.
@@ -127,7 +127,7 @@ Proof.
     - exact hs1.
 Qed.
 
-Theorem rule_while_is_valid A b c:
+Theorem while_intro_is_valid A b c:
   valid (Conj A b) c A ->
   valid A (While b c) (Conj A (bnot b)).
 Proof.
@@ -149,3 +149,44 @@ Proof.
       -- exact hs.
       -- simpl evB. rewrite heq. simpl. reflexivity.
 Qed.
+
+Theorem strengthen_precondition_is_valid {A1 A2 B c}:
+  (forall s, A2 s -> A1 s) -> valid A1 c B -> valid A2 c B.
+Proof.
+  intros h1 h2. unfold valid. intros s hs s1 hs1.
+  unfold sat in hs. apply (h1 s) in hs. clear h1.
+  unfold valid in h2. apply (h2 s).
+  * unfold sat. exact hs.
+  * exact hs1.
+Qed.
+
+Theorem weaken_postcondition_is_valid {A B1 B2 c}:
+  (forall s, B1 s -> B2 s) -> valid A c B1 -> valid A c B2.
+Proof.
+  intros h1 h2. unfold valid. intros s hs s1 hs1.
+  unfold sat. apply (h1 s1). clear h1.
+  fold (sat s1 B1). unfold valid in h2. apply (h2 s).
+  * exact hs.
+  * exact hs1.
+Qed.
+
+Module Example1.
+  Open Scope Z_scope.
+  Definition x: nat := 0.
+
+  (* {x = 0} x := x + 1 {x = 1} *)
+  Goal valid
+    (fun s => s x = 0)
+    (Assign x (add (loc x) (int 1)))
+    (fun s => s x = 1).
+  Proof.
+    assert (h1 := assign_intro_is_valid
+      (fun s => s x = 1) x (add (loc x) (int 1))).
+    unfold subst in h1. unfold variant in h1. simpl in h1.
+    assert (h2: forall s, s x = 0 -> s x + 1 = 1). {
+      intros s heq. rewrite <- (Z.add_cancel_r _ _ 1) in heq.
+      simpl in heq. exact heq.
+    }
+    exact (strengthen_precondition_is_valid h2 h1).
+  Qed.
+End Example1.
