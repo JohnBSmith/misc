91227d19a2911691a37cf36b90ed25aea0cbf976
finn <finn.bcd@gmail.com>
2025-03-10 10:47:57 +0100

    update

--- proofs/sets-nd/make.py
+++ proofs/sets-nd/make.py
@@ -2,24 +2,29 @@
 from subprocess import run
 from sys import argv, exit
 
-files = ["MK.txt", "logic.txt", "logic-eq.txt", "basic.txt", "relations.txt"]
+modules = [
+    ("MK", []),
+    ("logic", ["MK"]),
+    ("basic", ["MK", "logic"]),
+    ("relations", ["MK", "logic", "basic"])
+]
 
 if len(argv) == 2:
     if argv[1] == "-e":
-        for file in files:
-            run(["ndexport.py", file])
+        for (m, _) in modules:
+            run(["ndexport.py", m + ".txt"])
         exit(0)
     elif argv[1] == "-c":
         run(["ndexport.py", "--clear"])
         exit(0)
     else:
-        acc = []
-        for file in files:
-            if file != argv[1]:
-                acc.append("./lib/" + file)
-            else:
-                acc.append(file)
-                break
-        files = acc
+        modules = dict(modules)
+        dependencies = modules[argv[1]]
+        files = []
+        for m in dependencies:
+            files.append(f"./lib/{m}.txt")
+        files.append(argv[1] + ".txt")
+else:
+    files = [m + ".txt" for (m, _) in modules]
 
 run(["nd.py"] + files)
